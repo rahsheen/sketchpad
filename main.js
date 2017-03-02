4c07916a6aef03eab8948400269d0049914638f0
R. Anthony Porter <rah@sheenonline.biz>
2017-03-02 03:51:01 -0500

    Fixed grid size, calculated programmatically.
    Added styling and reset button.
    
    Removed borders and added background color, which resolved row wrapping errors.

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 $(function () {
-   for(var i=0;i<16;i++){
-        var row = $("<div></div>").addClass("row");
-       for(var j=0;j<16;j++) {
-           row.append("<div></div>");
-       }
-        $('.container').append(row);
-   }
 
+    function drawGrid(gridSpaces) {
+        var rowHeight = 500 / gridSpaces;
+        var colWidth = 500 / gridSpaces;
+
+        // Clear the sketchpad
+        $('#sketchpad').empty();
+
+        //Draw the grid with divs
+        for(var i=0;i<gridSpaces;i++){
+                var row = $("<div></div>").addClass("row").css("height",500/gridSpaces);
+            for(var j=0;j<gridSpaces;j++) {
+                var div = $("<div></div>").css("height", rowHeight).css("width", colWidth);
+                row.append(div);
+            }
+            $('#sketchpad').append(row);
+        }
+
+        //Activate sketchpad for drawing events
+        $( "#sketchpad .row div" ).hover(function() {
+  $( this ).addClass('blackBox');
+});
+    }
+
+drawGrid(16);
+
+$('#start-over').click(function(e) {
+    e.preventDefault();
+
+    var newGrid = prompt("How many spaces for next grid?");
+    drawGrid(newGrid);
+})
 });
